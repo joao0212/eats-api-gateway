74a0f5ffba0a78927a8f0f1e905c6be9e72a933f
João Victor <jvc.martins12@gmail.com>
2020-11-20 14:38:14 -0300

    Refactoring após inclusão de service registry

--- src/main/java/br/com/caelum/eatsapigateway/EatsApiGatewayApplication.java
+++ src/main/java/br/com/caelum/eatsapigateway/EatsApiGatewayApplication.java
@@ -2,9 +2,13 @@ package br.com.caelum.eatsapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EatsApiGatewayApplication {
 
--- src/main/java/br/com/caelum/eatsapigateway/configuration/FeignConfiguration.java
+++ src/main/java/br/com/caelum/eatsapigateway/configuration/FeignConfiguration.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 
+import feign.Logger;
 import feign.codec.Decoder;
 
 public class FeignConfiguration {
+
+	@Bean
+	Logger.Level feingLoggerLevel() {
+		return Logger.Level.FULL;
+	}
 
 	@Bean
 	public Decoder feignDecoder() {
